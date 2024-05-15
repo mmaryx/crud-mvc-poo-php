58ea8cb3bfc5725d9892ec5ac2c3cf258d33d66a
Marianna Ghiraldelli <marianna.ghiraldelli@scaffoldeducation.com.br>
2024-05-15 09:46:34 -0300

    Data Base Connection

--- configuration/connect.php
+++ configuration/connect.php
@@ -1,1 +1,23 @@
 <!-- Conexão com o banco de dados -->
+<?php
+    //Constantes necessárias
+    define('HOST', 'localhost');
+    define('DATABASENAME', 'crud-mvc');
+    define('USER', 'root');
+    define('PASSWORD', 'root');
+
+    class Connect {
+        protected $connection; //Atributo/variável que pode ser usada pela classe Connect e pelas classes que herdam dela
+        
+        function __construct() { //Método construtor
+            $this->connectDatabase();
+        }
+
+        function connectDatabase() {
+            try {
+                $this->connection = new PDO('mysql:host='.HOST.'dbname='.DATABASENAME, USER, PASSWORD);
+            } catch(PDOException $e) {
+                echo "Error!".$e->getMessage();
+            }
+        }
+    }
